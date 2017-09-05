af04b48094b9eec4f6eb6efd44ad7a3cc2a29592
Ales Justin <ales.justin@gmail.com>
2017-09-05 22:35:40 +0200

    Fix versions, add SI bean.

--- config/src/test/java/org/jboss/data/hibernatesearch/config/JpaConfiguration.java
+++ config/src/test/java/org/jboss/data/hibernatesearch/config/JpaConfiguration.java
@@ -1,8 +1,11 @@
 package org.jboss.data.hibernatesearch.config;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.spi.SearchIntegrator;
+import org.jboss.data.hibernatesearch.repository.config.EnableHibernateSearchRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +13,13 @@ import org.springframework.context.annotation.Configuration;
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 @Configuration
+@EnableHibernateSearchRepositories
 public class JpaConfiguration {
-  @Bean
-  public EntityManager createEntityManager() {
-    return Persistence.createEntityManagerFactory("hs").createEntityManager();
+  @PersistenceContext
+  EntityManager entityManager;
+
+  @Bean(destroyMethod = "close")
+  public SearchIntegrator searchIntegrator() {
+    return Search.getFullTextEntityManager(entityManager).getSearchFactory().unwrap(SearchIntegrator.class);
   }
 }
--- config/src/test/java/org/jboss/data/hibernatesearch/config/JpaTests.java
+++ config/src/test/java/org/jboss/data/hibernatesearch/config/JpaTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class JpaTests {
   @Test
-  @Ignore
+  @Ignore // TODO
   public void testDefault() {
 
   }
