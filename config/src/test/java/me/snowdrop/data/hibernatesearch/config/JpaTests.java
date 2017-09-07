dbab2cff1a0e071dfa440f7202384bc2b78a4a4b
Ales Justin <ales.justin@gmail.com>
2017-09-07 21:57:14 +0200

    Add license header.

--- config/src/test/java/me/snowdrop/data/hibernatesearch/config/JpaTests.java
+++ config/src/test/java/me/snowdrop/data/hibernatesearch/config/JpaTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.snowdrop.data.hibernatesearch.config;
 
 import me.snowdrop.data.hibernatesearch.TestUtils;
@@ -22,22 +38,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @EnableHibernateSearchRepositories(basePackageClasses = FruitHibernateSearchRepository.class)
 @EnableJpaRepositories(basePackageClasses = FruitRepository.class)
 public class JpaTests {
-    @Autowired
-    FruitHibernateSearchRepository hsRepository;
+  @Autowired
+  FruitHibernateSearchRepository hsRepository;
 
-    @Autowired
-    FruitRepository jpaRepository;
+  @Autowired
+  FruitRepository jpaRepository;
 
-    @Test
-    public void testDefault() {
-      Assert.assertNotNull(hsRepository);
-      Assert.assertNotNull(jpaRepository);
+  @Test
+  public void testDefault() {
+    Assert.assertNotNull(hsRepository);
+    Assert.assertNotNull(jpaRepository);
 
-      Assert.assertEquals(3, TestUtils.size(hsRepository.findAll()));
-      Assert.assertEquals(3, TestUtils.size(jpaRepository.findAll()));
+    Assert.assertEquals(3, TestUtils.size(hsRepository.findAll()));
+    Assert.assertEquals(3, TestUtils.size(jpaRepository.findAll()));
 
-      Fruit apple = hsRepository.findByName("Apple");
-      Assert.assertNotNull(apple);
-      Assert.assertEquals("Apple", apple.getName());
-    }
+    Fruit apple = hsRepository.findByName("Apple");
+    Assert.assertNotNull(apple);
+    Assert.assertEquals("Apple", apple.getName());
+  }
 }
--- core/src/test/java/me/snowdrop/data/hibernatesearch/ops/OpsRepository.java
+++ core/src/test/java/me/snowdrop/data/hibernatesearch/ops/OpsRepository.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.snowdrop.data.hibernatesearch.ops;
 
 import java.util.Collection;
@@ -11,21 +27,33 @@ import org.springframework.data.domain.Sort;
  */
 public interface OpsRepository extends HibernateSearchRepository<SimpleEntity, Long> {
   List<SimpleEntity> findByNameNot(String notName);
+
   List<SimpleEntity> findByNumberBetween(int min, int max);
+
   List<SimpleEntity> findByNumberLessThan(int number);
+
   List<SimpleEntity> findByNumberBefore(int number);
+
   List<SimpleEntity> findByNumberGreaterThan(int number);
+
   List<SimpleEntity> findByNumberAfter(int number);
 
   List<SimpleEntity> findByTextRegex(String text);
+
   List<SimpleEntity> findByTextLike(String text);
+
   List<SimpleEntity> findByTextStartingWith(String text);
+
   List<SimpleEntity> findByTextContaining(String text);
 
   List<SimpleEntity> findByTextNotContaining(String text);
+
   List<SimpleEntity> findByNameIn(Collection<String> names);
+
   List<SimpleEntity> findByNameNotIn(Collection<String> names);
+
   List<SimpleEntity> findByBuulTrue();
+
   List<SimpleEntity> findByBuulFalse();
 
   List<SimpleEntity> findByNameNot(String notName, Sort sort);
