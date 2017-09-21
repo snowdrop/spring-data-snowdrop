e942400a75bc0412ac03458ee0da0e2b67fea5d4
Yoann Rodière <yoann@hibernate.org>
2017-09-21 13:54:58 +0200

    Switch to a multiline import.sql

--- config/src/test/resources/import.sql
+++ config/src/test/resources/import.sql
@@ -1,10 +1,12 @@
-insert into fruit (name) values ('Cherry');
-insert into fruit (name) values ('Apple');
-insert into fruit (name) values ('Banana');
+insert into fruit (name) values
+    ('Cherry'),
+    ('Apple'),
+    ('Banana');
 
-insert into simple (name, text, number, buul, hero, color, latitude, longitude) values ('ann', 'Does Ann like good red apples?', -20, true, 'Superman', 'red', 10.0, 10.0)
-insert into simple (name, text, number, buul, hero, color, latitude, longitude) values ('barb', 'Why is Barb dancing twist?', -10, true, 'Spiderman', 'red', 24.0, 32.0)
-insert into simple (name, text, number, buul, hero, color, latitude, longitude) values ('carl', 'Carl is good at running and jumping.', 0, true, 'Flash', 'red', 20.0, 20.0)
-insert into simple (name, text, number, buul, hero, color, latitude, longitude) values ('doug', 'Doug likes to sleeps.', 10, false, 'Batman', 'black', -10.0, -10.0)
-insert into simple (name, text, number, buul, hero, color, latitude, longitude) values ('eva', 'Eva is running in circles.', 20, false, 'Ironman', 'gold', -20.0, 5.0)
-insert into simple (name, text, number, buul, hero, color, latitude, longitude) values ('fanny', 'Fanny is reading a good book.', 30, false, 'Aquaman', 'blue', 5.0, -20.0)
+insert into simple (name, text, number, buul, hero, color, latitude, longitude) values
+    ('ann', 'Does Ann like good red apples?', -20, true, 'Superman', 'red', 10.0, 10.0),
+    ('barb', 'Why is Barb dancing twist?', -10, true, 'Spiderman', 'red', 24.0, 32.0),
+    ('carl', 'Carl is good at running and jumping.', 0, true, 'Flash', 'red', 20.0, 20.0),
+    ('doug', 'Doug likes to sleeps.', 10, false, 'Batman', 'black', -10.0, -10.0),
+    ('eva', 'Eva is running in circles.', 20, false, 'Ironman', 'gold', -20.0, 5.0),
+    ('fanny', 'Fanny is reading a good book.', 30, false, 'Aquaman', 'blue', 5.0, -20.0);
